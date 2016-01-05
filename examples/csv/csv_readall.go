d239ee7186fcec95eaa7d51f07cc5a00c369047b
paavani <pavanisd9@gmail.com>
2016-01-05 11:49:04 +0530

    Display the the data line by line

--- examples/csv/csv_readall.go
+++ examples/csv/csv_readall.go
@@ -8,16 +8,14 @@ import(
 func main() {
   in := `Name,Price
          Carrot,"50"
-         Icecream,"30"` //don't put the gap between data and `  it will give error
+         Icecream,"30"` //don't give the gap between data and `  it will give error
     
       r :=csv.NewReader(strings.NewReader(in))
     
-  		record,err := r.ReadAll() //ReadAll reads all data at a time,no need to write EOF and for loop
+  		record,_:= r.ReadAll() 
 
-  		if err != nil {
-  			log.Println(err)
-  		}
-    // In single line display the data
-  		fmt.Println(record)
-	
+      for  i:= range record{
+      
+      fmt.Println(record[i]) //it will print the values line by line
+    }
   }
